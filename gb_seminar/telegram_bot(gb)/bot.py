dfd3491ca6e33a71e5a8504d6de2aa4ce921b286
Stefanishin-Maksim <stefanisihn-91@mail.ru>
2023-09-18 17:53:18 +0300

    Telegram Bot ( for 8 Seminar)

--- gb_seminar/telegram_bot(gb)/bot.py
+++ gb_seminar/telegram_bot(gb)/bot.py
@@ -10,21 +10,26 @@ bot = telebot.TeleBot(config.TOKEN)
 def process_add_film(message):
     with open("data.json", "r") as file:
         data = json.load(file)
-        data['movies'].append(message.text)
+        if message.text in data['movies']:
+            bot.send_message(message.chat.id, 'This film is already in the list . Try another')
 
-    with open("data.json", "w") as file:
-        json.dump(data, file, ensure_ascii=False)
-    bot.send_message(message.chat.id, 'Film added')
+
+        else:
+            data['movies'].append(message.text)
+            with open("data.json", "w") as file:
+                json.dump(data, file, ensure_ascii=False)
+            bot.send_message(message.chat.id, 'Film was added')
 
 
 def process_add_book(message):
     with open("data.json", "r") as file:
         data = json.load(file)
-        data['books'].append(message.text)
-
-    with open("data.json", "w") as file:
-        json.dump(data, file, ensure_ascii=False)
-    bot.send_message(message.chat.id, 'Book added')
+        if message.text in data['books']:
+            bot.send_message(message.chat.id, 'This book is already in the list . Try another')
+        else:
+            with open("data.json", "w") as file:
+                json.dump(data, file, ensure_ascii=False)
+            bot.send_message(message.chat.id, 'Book was added')
 
 
 def process_delete_film(message):
@@ -35,20 +40,20 @@ def process_delete_film(message):
         with open("data.json", "w", ) as file:
             json.dump(data, file, ensure_ascii=False)
 
-        bot.send_message(message.chat.id, 'Film deleted')
+        bot.send_message(message.chat.id, 'Film was erased')
     except:
         bot.send_message(message.chat.id, 'Oops. there is no such film. Try another')
 
 
 def process_delete_book(message):
     try:
-        with open("data.json", "r") as file:
+        with open('data.json', 'r', encoding='utf-8') as file:
             data = json.load(file)
         data['books'].remove(message.text)
-        with open("data.json", "w", ) as file:
+        with open('data.json', 'w', encoding='utf-8') as file:
             json.dump(data, file, ensure_ascii=False)
 
-        bot.send_message(message.chat.id, 'Book deleted')
+        bot.send_message(message.chat.id, 'Book was deleted')
     except:
         bot.send_message(message.chat.id, 'Oops. there is no such book. Try another')
 
@@ -62,8 +67,9 @@ def process_start_command(message):
     button1 = types.KeyboardButton("🎬 Random film ")
     button2 = types.KeyboardButton("📖 Random book")
     button3 = types.KeyboardButton("📖/🎬 All books and films")
+    button4 = types.KeyboardButton("Help")
 
-    keyboard.add(button1, button2, button3)
+    keyboard.add(button1, button2, button3, button4)
     bot.send_message(message.chat.id, "I'm - <b>{1.first_name}</b>, puppet-bot.".format(
         message.from_user, bot.get_me()),
                      parse_mode='html', reply_markup=keyboard)
@@ -77,24 +83,31 @@ def chatting(message):
             bot.send_message(message.chat.id, 'Bot film-keeper begins to work')
 
         elif message.text == '📖/🎬 All books and films':
-            with open('data.json', 'r') as file:
+            with open('data.json', 'r', encoding='utf-8') as file:
                 data = json.load(file)
-            bot.send_message(message.chat.id, str(data))
+                books = data['books']
+                movies = data['movies']
+                heading_books = "Books📖:\n"
+                heading_movies = "Movies🎬:\n"
+                result_books = heading_books + '\n'.join(books)
+                result_movies = heading_movies + '\n'.join(movies)
+
+            bot.send_message(message.chat.id, '\n' + result_books + '\n' + result_movies)
         elif message.text == '🎬 Random film':
-            with open("data.json", "r") as file:
+            with open('data.json', 'r', encoding='utf-8') as file:
                 data = json.load(file)
                 movies = data['movies']
             bot.send_message(message.chat.id, str(choice(movies)))
         elif message.text == '📖 Random book':
-            with open("data.json", "r") as file:
+            with open('data.json', 'r', encoding='utf-8') as file:
                 data = json.load(file)
                 books = data['books']
             bot.send_message(message.chat.id, str(choice(books)))
         elif message.text == 'add film':
-            bot.send_message(message.chat.id, 'Input added film 🎬')
+            bot.send_message(message.chat.id, 'Input title of film 🎬')
             bot.register_next_step_handler(message, process_add_film)
         elif message.text == 'add book':
-            bot.send_message(message.chat.id, 'Input added book 📖')
+            bot.send_message(message.chat.id, 'Type  a title of book 📖')
             bot.register_next_step_handler(message, process_add_book)
         elif message.text == 'delete film':
             bot.send_message(message.chat.id, 'Input name of erased film 🎬')
@@ -102,6 +115,25 @@ def chatting(message):
         elif message.text == 'delete book':
             bot.send_message(message.chat.id, 'Input name of erased book 📖')
             bot.register_next_step_handler(message, process_delete_book)
+        elif message.text == 'Help':
+            with open('data.json', 'r', encoding='utf-8') as file:
+                data = json.load(file)
+                commands = data['commands']
+            bot.send_message(message.chat.id, '\n'.join(commands))
+        elif message.text == 'show movies':
+            with open('data.json', 'r', encoding='utf-8') as file:
+                data = json.load(file)
+                movies = data['movies']
+                heading_movies = "Movies🎬:\n"
+                result_movies = heading_movies + '\n'.join(movies)
+            bot.send_message(message.chat.id, '\n' + result_movies)
+        elif message.text == 'show books':
+            with open('data.json', 'r', encoding='utf-8') as file:
+                data = json.load(file)
+                movies = data['books']
+                heading_books = "Books📖:\n"
+                result_books = heading_books + '\n'.join(movies)
+            bot.send_message(message.chat.id, '\n' + result_books)
         else:
             bot.send_message(message.chat.id, "Don't know how to answer 😢")
 
